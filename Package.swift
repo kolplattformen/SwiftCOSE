452953243879aa04dff7c8f4ede72a01e25a3050
Erik Hellman <erik.hellman@hellsoft.se>
2021-04-13 08:54:10 +0200

    Added base45 extension to String

--- Package.swift
+++ Package.swift
@@ -14,6 +14,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/unrelentingtech/SwiftCBOR", from: "0.4.3"),
+        .package(url: "https://github.com/grpc/grpc-swift", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/SwiftCOSE/SwiftCOSE.swift
+++ Sources/SwiftCOSE/SwiftCOSE.swift
@@ -1,3 +1,34 @@
+@testable import SwiftCBOR
+@testable import GRPC
+@testable import NIO
+
 struct SwiftCOSE {
     var text = "Hello, World!"
+}
+
+extension String {
+    func fromBase45()->Data {
+        let BASE45_CHARSET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:"
+        var d = Data()
+        var o = Data()
+        
+        for c in self {
+            if let at = BASE45_CHARSET.firstIndex(of: c) {
+                let idx  = BASE45_CHARSET.distance(from: BASE45_CHARSET.startIndex, to: at)
+                d.append(UInt8(idx))
+                }
+        }
+        for i in stride(from:0, to:d.count, by: 3) {
+            var x : UInt32 = UInt32(d[i]) + UInt32(d[i+1])*45
+            if (d.count - i >= 3) {
+                x += 45 * 45 * UInt32(d[i+2])
+                o.append(UInt8(x / 256))
+                o.append(UInt8(x % 256))
+            } else {
+                o.append(UInt8(x % 256))
+            }
+        }
+        return o
+    }
+
 }
